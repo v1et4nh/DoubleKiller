35362f15c1c2651744a272dd2504cf93a514c9ee
v1et4nh <v1et4nh@googlemail.com>
2021-01-27 00:36:38 +0100

    Added label

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2.cv2 as cv2  # pycharm specific
 import os
 import tkinter as tk
 
-from tkinter import messagebox, filedialog
+from tkinter import messagebox, filedialog, ttk
 
 
 class App(tk.Tk):
@@ -15,6 +15,7 @@ class App(tk.Tk):
         self.title = 'DoubleKiller'
         self.folder = ''
 
+        # 1.Step: Select folder
         ROW += 1
         self.text1 = tk.Label(self, text='1. Select folder')
         self.text1.grid(row=ROW, column=0)
@@ -23,6 +24,17 @@ class App(tk.Tk):
         # self.EntrySelectFolder.grid(row=ROW, column=0)
         self.BtnSelectFolder = tk.Button(self, text='Open Folder', command=self.openFolder)
         self.BtnSelectFolder.grid(row=ROW, column=0)
+
+        # 2.Step: Select depth
+        ROW += 1
+        depthList = ("Same level", "Include all subfolder")
+        variable = tk.StringVar(self)
+        variable.set(depthList[0])
+        self.combDepth = ttk.Combobox(self, textvariable=variable, state='readonly')
+        self.combDepth['value'] = depthList
+        self.combDepth.grid(row=ROW, column=0)
+        self.combDepth.current(0)
+
 
     def openFolder(self):
         self.folder = filedialog.askdirectory(title='Select Folder')
