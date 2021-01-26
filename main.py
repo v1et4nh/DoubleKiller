0186be993e6e441ab20f966a52e1c812dd021519
v1et4nh <v1et4nh@googlemail.com>
2021-01-26 23:55:13 +0100

    Set up GUI

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
+import cv2.cv2 as cv2  # pycharm specific
+import os
 import tkinter as tk
 
+from tkinter import messagebox, filedialog
 
-print('hello')
+
+class App(tk.Tk):
+    def __init__(self):
+        # Set up Main Window
+        tk.Tk.__init__(self)
+        # Initialize parameters
+        ROW = 0
+
+        self.folder = ''
+
+        ROW += 1
+        # self.EntrySelectFolder = tk.Entry(self, width=44)
+        # self.EntrySelectFolder.grid(row=ROW, column=0)
+        self.BtnSelectFolder = tk.Button(self, text='Open Folder', command=self.openFolder)
+        self.BtnSelectFolder.grid(row=ROW, column=0)
+
+    def openFolder(self):
+        self.folder = filedialog.askdirectory(title='Select Folder')
+
+
+def openFile(file):
+    img = cv2.imread(file)
+    return img
+
+
+def showImg(img):
+    cv2.imshow('', img)
+    cv2.waitKey(0)
+
+
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()
